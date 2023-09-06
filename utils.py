fd77df1afd95e1f6ed2612796161df3fb972aa67
Josh Hope-Collins <jhc.jss@gmail.com>
2023-09-06 10:22:51 -0700

    print_once function

--- utils.py
+++ utils.py
@@ -6,3 +6,9 @@ def print_in_order(comm, string):
             print(string)
         comm.Barrier()
     comm.Barrier()
+
+def print_once(comm, string):
+    comm.Barrier()
+    if comm.rank == 0:
+        print(string)
+    comm.Barrier()
