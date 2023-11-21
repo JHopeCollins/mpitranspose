4d5c051c4db6cf53ccfc35ac1ed6665c40da2e69
Josh Hope-Collins <jhc.jss@gmail.com>
2023-11-21 15:30:08 +0000

    remove old files

--- timer_context.py
+++ timer_context.py
@@ -1,9 +1,10 @@
 from contextlib import contextmanager
-from mpi4py import MPI 
+from mpi4py import MPI
+
 
 @contextmanager
 def mpi_timer(name, stream=print):
-    stream(f"{name} starting...")                                                                                                                             
+    stream(f"{name} starting...")
     stime = MPI.Wtime()
     yield
     etime = MPI.Wtime()
