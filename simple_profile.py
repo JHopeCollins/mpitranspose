5e4795817ce339f1c3bc34c05b540220b4d0b24b
JHopeCollins <joshua.hope-collins13@imperial.ac.uk>
2023-11-21 17:40:59 +0000

    update simple test

--- simple_profile.py
+++ simple_profile.py
@@ -1,6 +1,6 @@
 import numpy as np
 from mpi4py import MPI
-from transpose import Transposerv as Transposer
+from transpose import Transposer as Transposer
 from timer_context import measure_timing
 from utils import print_once
 from functools import partial
@@ -11,10 +11,11 @@ size = comm.size
 
 mpiprint = partial(print_once, comm)
 
-xdofs = int(50e3)
-ydofs = 1
+xdofs = int(500e3)
+ydofs = 2
 
 mpiprint(f"Total dofs = {size*xdofs*ydofs}")
+mpiprint(f"Transfer type: {Transposer.__name__}")
 
 xpart = tuple((xdofs for _ in range(comm.size)))
 ypart = tuple((ydofs for _ in range(comm.size)))
@@ -31,16 +32,16 @@ np.random.seed(12345)
 
 x[:] = np.random.rand(*x.shape)
 
-nmeasure = 4
-nwarmup = 1
+nwarmup = 2
+nmeasure = 10
 
-fmeasurements = measure_timing(partial(transpose.forward, x, y),
+fmeasurements = measure_timing(lambda: transpose.forward(x, y),
                                nmeasure=nmeasure, nwarmup=nwarmup)
 mean = round(fmeasurements.mean, 4)
 std = round(fmeasurements.std, 4)
 mpiprint(f"{nmeasure} forward  iterations: mean = {mean}, std = {std}")
 
-bmeasurements = measure_timing(partial(transpose.backward, y, x),
+bmeasurements = measure_timing(lambda: transpose.backward(y, x),
                                nmeasure=nmeasure, nwarmup=nwarmup)
 mean = round(bmeasurements.mean, 4)
 std = round(bmeasurements.std, 4)
